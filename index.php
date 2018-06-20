0c7a58999a2919ebe2418963a359f034f4cadbda
MatheusDosReis <matheusdrdj@gmail.com>
2018-06-19 21:38:17 -0300

    Aprimorando header

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html>
-
-<script src='src/js/jquery-3.3.1.min.js'></script>
-<script src='src/js/bootstrap.bundle.min.js'></script>
-<script src='src/js/bootstrap.min.js'></script>
-<script src='src/js/popper.min.js'></script>
-<link rel='stylesheet' href='src/css/style.css'>
-<link rel='stylesheet' href='src/css/bootstrap.min.css'>
-<link rel='stylesheet' href='src/css/bootstrap-grid.min.css'>
-<link rel='stylesheet' href='src/css/bootstrap-reboot.min.css'>
-
+<head>
+    <title> SAD - Sistema de Avaliação de Disciplinas </title>
+    <script src='src/js/jquery-3.3.1.min.js'></script>
+    <script src='src/js/bootstrap.bundle.min.js'></script>
+    <script src='src/js/bootstrap.min.js'></script>
+    <script src='src/js/popper.min.js'></script>
+    <link rel='stylesheet' href='src/css/style.css'>
+    <link rel='stylesheet' href='src/css/bootstrap.min.css'>
+    <link rel='stylesheet' href='src/css/bootstrap-grid.min.css'>
+    <link rel='stylesheet' href='src/css/bootstrap-reboot.min.css'>
+</head>
 <body>
     <header> 
         <?php require 'views/header.php'; ?>
--- views/header.php
+++ views/header.php
@@ -4,16 +4,25 @@
 
 ?>
 
+<style>
+    .nav-link{
+        color: rgba(255,255,255,.5)
+    }
+    .nav-link:hover{
+        color: rgba(255,255,255,.8)
+    }
+</style>
+
 <nav class='navbar navbar-expand-lg navbar-dark bg-dark' align='center'>
-    <a class='navbar-brand' href='/'> Sistema de avaliação de disciplinas </a>
+    <a class='navbar-brand' href='/'> SAD </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" 
             data-target="#navbarText" aria-controls="navbarText" 
             aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class='collapse navbar-collapse'>
+    <div id='navbarText' class='collapse navbar-collapse'>
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
+            <li class="nav-item">
                 <a class="nav-link" href="?page=home">
                     Página Inicial <span class="sr-only">(current)</span>
                 </a>
@@ -21,9 +30,9 @@
             <li class="nav-item">
                 <a class="nav-link" href="?page=disciplinas">Disciplinas</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?page=user">Meu usuário</a>
-            </li>
         </ul>
+        <div>
+            <a class="nav-link" href="?page=user">Minha conta</a>
+        </div>
     </div>
 </nav>
