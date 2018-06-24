1d755b026ff402ca484cd60c8c6072882c25987a
MatheusDosReis <matheusdrdj@gmail.com>
2018-06-24 16:28:22 -0300

    Separando tags no header

--- views/header.php
+++ views/header.php
@@ -2,17 +2,18 @@
 
 <head>
         <title> SAD - Sistema de Avaliação de Disciplinas </title>
+        <link rel='shorcut icon' type='image/x-icon' href='./favicon.ico'>
+        
         <script src='../src/js/jquery-3.3.1.min.js'></script>
         <script src='../src/js/bootstrap.bundle.min.js'></script>
         <script src='../src/js/bootstrap.min.js'></script>
         <script src='../src/js/popper.min.js'></script>
 
-
-        <link rel='shorcut icon' type='image/x-icon' href='./favicon.ico'>
         <link rel='stylesheet' href='../src/css/style.css'>
         <link rel='stylesheet' href='../src/css/bootstrap.min.css'>
         <link rel='stylesheet' href='../src/css/bootstrap-grid.min.css'>
         <link rel='stylesheet' href='../src/css/bootstrap-reboot.min.css'>
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
