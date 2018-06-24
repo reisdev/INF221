b53891e26858595014c1b2accffda7a9043574fa
MatheusDosReis <matheusdrdj@gmail.com>
2018-06-23 23:18:09 -0300

    Pesquisa implementada e arrumando layout

--- pages/disciplinas.php
+++ pages/disciplinas.php
@@ -6,22 +6,37 @@
         $disciplinas = [
             [ 'nome' => 'Cálculo I', 'codigo' => 'MAT140', 'alunos' => '450'],
             [ 'nome' => 'Programação I', 'codigo' => 'INF110', 'alunos' => '60'],
-            [ 'nome' => 'Engenharia de Software I', 'codigo' => 'INF221', 'alunos' => '36']
+            [ 'nome' => 'Engenharia de Software I', 'codigo' => 'INF221', 'alunos' => '36'],
+            [ 'nome' => 'Física I', 'codigo' => 'FIS201', 'alunos' => '76'],
+            [ 'nome' => 'Organização de Computadores I', 'codigo' => 'INF251', 'alunos' => '40']
         ];
     ?>
     <body>
-        <div class='d-flex container justify-content-center'>
+        <div class='container-fluid'>
             <?php
+                try {
+                    if(isset($_POST['nome']) && $_POST['nome'] !== ""){
+                        $disciplinas = array_filter( $disciplinas, function ( $item ) {
+                            if(stripos($item['nome'], $_POST['nome']) !== false || stripos($item['codigo'], $_POST['nome']) !== false) {
+                                return true;
+                            }
+                            return false;
+                        });
+                    }
+                }
+                catch(Exception $error){
+                    echo $error;
+                }
                 foreach($disciplinas as $item){
                     echo "
                         <div class='card card-disciplina'>
-                            <div class='card-body'>
+                            <div class='d-flex flex-column card-body mt-auto'>
                                 <h5 class='card-title'> {$item['nome']} </h5>
                                 <div class='card-text'>
                                     <p> <b>Código:</b> {$item['codigo']} </p>
                                     <p> <b>Alunos matrículados</b> {$item['alunos']} </p>
                                 </div>
-                                <a class='card-link btn btn-primary' data-toggle='collapse' role='button' data-target='#{$item['codigo']}'>Avaliar</a>
+                                <a class='mt-auto card-link btn btn-primary' data-toggle='collapse' role='button' data-target='#{$item['codigo']}' style='float: 'bottom' '>Avaliar</a>
                                 <div style='margin: 0px 0px 30px 0px' id='{$item['codigo']}' class='collapse' aria-expanded='true' aria-labelledby='{$item['codigo']}'>
                                 Avaliação: <br>
                                 <img src='/INF221/src/images/rating-star.jpg' width='120' class='img-fluid' href='?page=user'> <br>
