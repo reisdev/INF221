3f448e22f0070cba75f470eb98bf351b1cefbb9d
Jnnsm <eduardo.j.moreira@ufv.br>
2018-06-24 13:22:13 -0300

    Amigos Conectados

--- pages/user.php
+++ pages/user.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <?php
     $usuarios = [
-        [ 'nome' => 'Matheus Paulista', 'birthday' => '20-06-2018', 'CPF' => '1921548239102'],
-        [ 'nome' => 'Matheus Negro', 'birthday' => '20-06-2018', 'CPF' => '1921548239102']
+        [ 'nome' => 'Matheus Paulista', 'birthday' => '20-06-2018', 'CPF' => '1921548239102', 'foto' => "../src/images/profile2.jpg"],
+        [ 'nome' => 'Matheus Negro', 'birthday' => '21-06-2018', 'CPF' => '1921548239102', 'foto' => "../src/images/profile3.jpg"],
+        [ 'nome' => 'Matheus Negro', 'birthday' => '22-06-2018', 'CPF' => '1921548239102', 'foto' => "../src/images/profile3.jpg"],
+        [ 'nome' => 'Matheus Negro', 'birthday' => '23-06-2018', 'CPF' => '1921548239102', 'foto' => "../src/images/profile3.jpg"]
     ];
 ?>
 
@@ -28,31 +30,45 @@
             </div>
 
             <br><p class='display-4' style='font-size: 40px'> Amigos Conectados </p> <br>
-            <?php
-                $i = 2;
-                foreach($usuarios as $item){
-                    echo "
-                        <div class='card card-perfil'>
-                            <div class='card-body' width='100%'>
-                                    <div class='profile-header-img'>
-                                        <img class='img-circle' src='../src/images/profile".$i.".jpg' />
-                                        <div style='vertical-align: middle'>
-                                            <p><b>Nome do Usuário:</b> {$item['nome']} </p>
-                                            <p><b>Data de nascimento:</b> {$item['birthday']}</p>
-                                            <p><b>ID do Facebook:</b> {$item['CPF']}</p>
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class='img-circle' style='height:100px' src="../src/images/profile.jpg" alt="First slide">
+                    </div>
+                        <?php
+                            $counter = 0;
+                            foreach($usuarios as $item){
+
+                                echo "<div class='carousel-item'>";
+                                echo "
+                                <div class='card card-perfil' >
+                                    <div >
+                                        <div class='card-body'>
+                                            <img class='img-circle' style='height:100px' src='{$item['foto']}' />
+                                            <div style='vertical-align: middle'>
+                                                <p><b>Nome do Usuário:</b> {$item['nome']} </p>
+                                                <p><b>Data de nascimento:</b> {$item['birthday']}</p>
+                                                <p><b>ID do Facebook:</b> {$item['CPF']}</p>
+                                            </div>
                                         </div>
                                     </div>
-                            </div>
-                        </div>
+                                </div>
+                                ";
 
-                        ";
-                    $i = $i+1;
-                }
-            ?>
+                                echo "</div>";
+                            }
+
+                        ?>
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true" style="background-color:black"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true" style="background-color:black"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                </div>
+            </div>
         </div>
-
-
-
-
     </body>
 </html>
