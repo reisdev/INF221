3d061e6629fcbb55f7622f64b442fe3c9fee6bed
MatheusDosReis <matheusdrdj@gmail.com>
2018-06-24 16:03:51 -0300

    Aprimorando style

--- pages/user.php
+++ pages/user.php
@@ -29,112 +29,56 @@
             $usuario = ['nome' => 'Eduardo Moreira Júnior','birthday' => '20-06-2018', 'CPF' => '1921548239102'];
         ?>
         <div class='container-fluid'>
-            <p class='display-4' style='font-size: 40px'> Dados do usuário </p> <br>
-            <div class='card'>
-                <div class='card-body'>
-                        <div class="profile-header-img">
-                            <img class="img-circle" src="../src/images/profile.jpg" />
-                            <div style='vertical-align: middle'>
-                                <p><b>Nome do Usuário:</b> <?php echo $usuario['nome']; ?></p>
-                                <p><b>Data de nascimento:</b> <?php echo $usuario['birthday']; ?></p>
-                                <p><b>ID do Facebook:</b> <?php echo $usuario['CPF']; ?></p>
-                            </div>
-                        </div>
-                </div>
-            </div>
-
-            <!-- Minhas Avaliações -->
-            <div class='row'>
-                <div class='col-lg-6'>
-                    <br><p class='display-4' style='font-size: 40px'> Suas Avaliações </p> <br>
-                    <div id="carouselAvaliacao" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <?php
-                                $count = 0;
-                                foreach ($avaliacoes as $item) {
-                                    $active = "class='active'";
-                                    echo "<li class='slider-selector' data-target='#carouselAvaliacao' data-slide-to='". $count ."' ".$active."></li>";
-                                    $count++;
-                                    $active = "";
-                                }
-                            ?>
-                        </ol>
-                        <div class="carousel-inner">
-                        <?php
-                            $active = " active";
-                            foreach($avaliacoes as $item){
-                                echo "<div class='carousel-item" . $active. "'>";
-                                echo "
-                                <div class='card card-avaliacao-perfil' >
-                                    <div>
-                                        <div class='card-body'>
-                                            <h5 class='card-title' align='center'> {$item['nome']} </h5>
-                                            <div class='card-text' style='vertical-align: middle'>
-                                                <div class='row'>
-                                                    <div class='col-md-6'>
-                                                        <p> <b>Código:</b> {$item['codigo']} </p>
-                                                        <p> <b>Nota Dada:</b> {$item['nota']} </p>
-                                                        <p> <b>É difícil?:</b> {$item['dificuldade']} </p>
-                                                    </div>
-                                                    <div class='col-md-6'>
-                                                        <p> <b>É útil?:</b> {$item['util']} </p>
-                                                        <p> <b>Recomenda?:</b> {$item['recomenda']} </p>
-                                                    </div>
-                                                </div>
-                                                <div align='center'> <a  href='/INF221/pages/avaliacao_disciplina.php?disciplina={$item["codigo"]}' class='mt-auto card-link btn btn-primary' role='button' style='float: 'bottom' '>Editar</a>
-                                            </div></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                ";
-                                echo "</div>";
-                                $active = "";
-                            }
-                        ?>
-                            <a class="carousel-control-prev" href="#carouselAvaliacao" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#carouselAvaliacao" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>
+            <div class='card card-body'>
+                <p class='display-4' style='font-size: 40px'> Dados do usuário </p> <br>
+                <div class="profile-header-img">
+                    <img class="img-circle" src="../src/images/profile.jpg" />
+                    <div style='vertical-align: middle'>
+                        <p><b>Nome do Usuário:</b> <?php echo $usuario['nome']; ?></p>
+                        <p><b>Data de nascimento:</b> <?php echo $usuario['birthday']; ?></p>
+                        <p><b>ID do Facebook:</b> <?php echo $usuario['CPF']; ?></p>
                     </div>
                 </div>
-                <!-- Amigos Conectados -->
-                <div class='col-lg-6'>
-                    <br><p class='display-4' style='font-size: 40px'> Amigos Conectados </p> <br>
-                    <div id="carouselFriends" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <?php
-                                $count = 0;
-                                foreach ($usuarios as $item) {
-                                    $active = "class='active'";
-                                    echo "<li class='slider-selector' data-target='#carouselFriends' data-slide-to='". $count ."' ".$active."></li>";
-                                    $count++;
-                                    $active = "";
-                                }
-                            ?>
-                        </ol>
-                        <div class="carousel-inner">
+                <!-- Minhas Avaliações -->
+                <div class='row'>
+                    <div class='col-lg-6'>
+                        <br><p class='display-4' style='font-size: 40px'> Suas Avaliações </p> <br>
+                        <div id="carouselAvaliacao" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <?php
+                                    $count = 0;
+                                    foreach ($avaliacoes as $item) {
+                                        $active = "class='active'";
+                                        echo "<li class='slider-selector' data-target='#carouselAvaliacao' data-slide-to='". $count ."' ".$active."></li>";
+                                        $count++;
+                                        $active = "";
+                                    }
+                                ?>
+                            </ol>
+                            <div class="carousel-inner">
                             <?php
                                 $active = " active";
-                                foreach($usuarios as $item){
+                                foreach($avaliacoes as $item){
                                     echo "<div class='carousel-item" . $active. "'>";
                                     echo "
-                                    <div class='card card-perfil' >
-                                        <div >
+                                    <div class='card card-avaliacao-perfil' >
+                                        <div style='padding: 10px 20px 20px 30px'>
                                             <div class='card-body'>
-                                                <div class='profile-header-img'>
-                                                    <img class='img-circle' src='{$item['foto']}' />
-                                                    <div style='vertical-align: middle'>
-                                                        <p><b>Nome do Usuário:</b> {$item['nome']} </p>
-                                                        <p><b>Data de nascimento:</b> {$item['birthday']}</p>
-                                                        <p><b>ID do Facebook:</b> {$item['CPF']}</p>
-                                                        <a href='/INF221/pages/profile.php?nome={$item['nome']}' class='mt-auto card-link btn btn-primary' role='button' style='float: 'bottom' '>Ver Perfil</a>
+                                                <h5 class='card-title' align='center'> {$item['nome']} </h5>
+                                                <div class='card-text' style='vertical-align: middle'>
+                                                    <div class='row'>
+                                                        <div class='col-md-6'>
+                                                            <p> <b>Código:</b> {$item['codigo']} </p>
+                                                            <p> <b>Nota Dada:</b> {$item['nota']} </p>
+                                                            <p> <b>É difícil?:</b> {$item['dificuldade']} </p>
+                                                        </div>
+                                                        <div class='col-md-6'>
+                                                            <p> <b>É útil?:</b> {$item['util']} </p>
+                                                            <p> <b>Recomenda?:</b> {$item['recomenda']} </p>
+                                                        </div>
                                                     </div>
-                                                </div>
+                                                    <div align='center'> <a  href='/INF221/pages/avaliacao_disciplina.php?disciplina={$item["codigo"]}' class='mt-auto card-link btn btn-primary' role='button' style='float: 'bottom' '>Editar</a>
+                                                </div></div>
                                             </div>
                                         </div>
                                     </div>
@@ -143,14 +87,67 @@
                                     $active = "";
                                 }
                             ?>
-                            <a class="carousel-control-prev" href="#carouselFriends" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#carouselFriends" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
+                                <a class="carousel-control-prev" href="#carouselAvaliacao" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselAvaliacao" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Amigos Conectados -->
+                    <div class='col-lg-6'>
+                        <br><p class='display-4' style='font-size: 40px'> Amigos Conectados </p> <br>
+                        <div id="carouselFriends" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <?php
+                                    $count = 0;
+                                    foreach ($usuarios as $item) {
+                                        $active = "class='active'";
+                                        echo "<li class='slider-selector' data-target='#carouselFriends' data-slide-to='". $count ."' ".$active."></li>";
+                                        $count++;
+                                        $active = "";
+                                    }
+                                ?>
+                            </ol>
+                            <div class="carousel-inner">
+                                <?php
+                                    $active = " active";
+                                    foreach($usuarios as $item){
+                                        echo "<div class='carousel-item" . $active. "'>";
+                                        echo "
+                                        <div class='card card-perfil' >
+                                            <div >
+                                                <div class='card-body'>
+                                                    <div class='profile-header-img'>
+                                                        <img class='img-circle' src='{$item['foto']}' />
+                                                        <div style='vertical-align: middle'>
+                                                            <p><b>Nome do Usuário:</b> {$item['nome']} </p>
+                                                            <p><b>Data de nascimento:</b> {$item['birthday']}</p>
+                                                            <p><b>ID do Facebook:</b> {$item['CPF']}</p>
+                                                            <a href='/INF221/pages/profile.php?nome={$item['nome']}' class='mt-auto card-link btn btn-primary' role='button' style='float: 'bottom' '>Ver Perfil</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        ";
+                                        echo "</div>";
+                                        $active = "";
+                                    }
+                                ?>
+                                <a class="carousel-control-prev" href="#carouselFriends" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselFriends" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
