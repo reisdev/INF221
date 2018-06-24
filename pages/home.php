78052f5daac7fef22b90564ca871bb8cad1af5a2
Mateus Armond <mateus.armond@outlook.com>
2018-06-23 23:33:54 -0300

    Ranking

--- pages/home.php
+++ pages/home.php
@@ -3,11 +3,49 @@
 <html>
     <?php include('../views/header.php'); ?>
     <body>
-        <?php
-            $texto = 'SEJA BEM VINDO';
-        ?>
+    <h2 align="center"> Ranking: </h2>
 
-        <div class='container'>
-            <?php echo $texto; ?>
+    <div class='container'>
+        <div align="center" class='card card-disciplina'>
+            <div class='card-body'>
+                <h5 class='card-title'> Recomendadas </h5>
+                <div class='card-text'>
+                    <p> <b>1.</b> INF110 </p>
+                    <p> <b>2.</b> INF350 </p>
+                    <p> <b>3.</b> MAT147 </p>
+                    <p> <b>4.</b> MAT131 </p>
+                    <p> <b>5.</b> INF390 </p>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+        <div align="center" class='card card-disciplina'>
+            <div class='card-body'>
+                <h5 class='card-title'> Fáceis </h5>
+                <div class='card-text'>
+                    <p> <b>1.</b> INF110 </p>
+                    <p> <b>2.</b> INF350 </p>
+                    <p> <b>3.</b> MAT147 </p>
+                    <p> <b>4.</b> MAT131 </p>
+                    <p> <b>5.</b> INF390 </p>
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <div align="center" class='card card-disciplina'>
+            <div class='card-body'>
+                <h5 class='card-title'> Úteis </h5>
+                <div class='card-text'>
+                    <p> <b>1.</b> INF110 </p>
+                    <p> <b>2.</b> INF350 </p>
+                    <p> <b>3.</b> MAT147 </p>
+                    <p> <b>4.</b> MAT131 </p>
+                    <p> <b>5.</b> INF390 </p>
+                </div>
+                </div>
+            </div>
         </div>
     </body>
