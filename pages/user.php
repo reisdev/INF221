2be1c787fc08e86f304cc10c891271287308af9f
Jnnsm <eduardo.j.moreira@ufv.br>
2018-06-24 02:06:44 -0300

    Amigos conectados porém não responsivos

--- pages/user.php
+++ pages/user.php
@@ -12,7 +12,7 @@
         <?php
             $usuario = ['nome' => 'Eduardo Moreira Júnior','birthday' => '20-06-2018', 'CPF' => '1921548239102'];
         ?>
-        <div class='container'>
+        <div class='container-fluid'>
             <p class='display-4' style='font-size: 40px'> Dados do usuário </p> <br>
             <div class='card'>
                 <div class='card-body' width='100%'>
@@ -26,28 +26,33 @@
                         </div>
                 </div>
             </div>
-        </div>
-        <div class='container'>
-        <?php
-            $i = 2;
-            foreach($usuarios as $item){
-                echo "
-                    <div class='card card-perfil'>
-                        <div class='card-body' width='100%'>
-                                <div class='profile-header-img'>
-                                    <img class='img-circle' src='../src/images/profile".$i.".jpg' />
-                                    <div style='vertical-align: middle'>
-                                        <p><b>Nome do Usuário:</b> {$item['nome']} </p>
-                                        <p><b>Data de nascimento:</b> {$item['birthday']}</p>
-                                        <p><b>ID do Facebook:</b> {$item['CPF']}</p>
+
+            <br><p class='display-4' style='font-size: 40px'> Amigos Conectados </p> <br>
+            <?php
+                $i = 2;
+                foreach($usuarios as $item){
+                    echo "
+                        <div class='card card-perfil'>
+                            <div class='card-body' width='100%'>
+                                    <div class='profile-header-img'>
+                                        <img class='img-circle' src='../src/images/profile".$i.".jpg' />
+                                        <div style='vertical-align: middle'>
+                                            <p><b>Nome do Usuário:</b> {$item['nome']} </p>
+                                            <p><b>Data de nascimento:</b> {$item['birthday']}</p>
+                                            <p><b>ID do Facebook:</b> {$item['CPF']}</p>
+                                        </div>
                                     </div>
-                                </div>
+                            </div>
                         </div>
-                    </div>
-                    ";
-                $i = $i+1;
-            }
-        ?>
+
+                        ";
+                    $i = $i+1;
+                }
+            ?>
         </div>
+
+
+
+
     </body>
 </html>
