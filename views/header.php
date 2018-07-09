f678e281ba4455cff7c1d3b4b088efea000f981b
Jnnsm <eduardo.j.moreira@ufv.br>
2018-07-09 09:20:04 -0300

    Facebook Login

--- views/header.php
+++ views/header.php
@@ -20,75 +20,77 @@
 </head>
 
 <script>
-// This is called with the results from from FB.getLoginStatus().
-function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-        // Logged into your app and Facebook.
-        testAPI();
-    } else {
-        // The person is not logged into your app or we are unable to tell.
+    // This is called with the results from from FB.getLoginStatus().
+    function statusChangeCallback(response) {
+        console.log('statusChangeCallback');
+        console.log(response);
+        // The response object is returned with a status field that lets the
+        // app know the current login status of the person.
+        // Full docs on the response object can be found in the documentation
+        // for FB.getLoginStatus().
+        if (response.status === 'connected') {
+            document.getElementById('account').style.visibility='visible';
+            testAPI();
+        } else {
+            document.getElementById('account').style.visibility='hidden';
+            // The person is not logged into your app or we are unable to tell.
+        }
     }
-}
 
-// This function is called when someone finishes with the Login
-// Button.  See the onlogin handler attached to it in the sample
-// code below.
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
+    // This function is called when someone finishes with the Login
+    // Button.  See the onlogin handler attached to it in the sample
+    // code below.
+    function checkLoginState() {
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+        });
+    }
+
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '1843440822387020',
+            cookie     : true,  // enable cookies to allow the server to access
+            // the session
+            xfbml      : true,  // parse social plugins on this page
+            version    : 'v2.8' // use graph api version 2.8
+        });
+
+        // Now that we've initialized the JavaScript SDK, we call
+        // FB.getLoginStatus().  This function gets the state of the
+        // person visiting this page and can return one of three states to
+        // the callback you provide.  They can be:
+        //
+        // 1. Logged into your app ('connected')
+        // 2. Logged into Facebook, but not your app ('not_authorized')
+        // 3. Not logged into Facebook and can't tell if they are logged into
+        //    your app or not.
+        //
+        // These three cases are handled in the callback function.
+
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+        });
+
+    };
+
+    // Load the SDK asynchronously
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    // Here we run a very simple test of the Graph API after login is
+    // successful.  See statusChangeCallback() for when this call is made.
+    function testAPI() {
+        FB.api('/me', function(response) {
+            //alert('Logado com sucesso: ' + response.name);
+        });
+    }
+    FB.logout(function(response) {
     });
-}
-
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '1843440822387020',
-        cookie     : true,  // enable cookies to allow the server to access
-        // the session
-        xfbml      : true,  // parse social plugins on this page
-        version    : 'v2.8' // use graph api version 2.8
-    });
-
-    // Now that we've initialized the JavaScript SDK, we call
-    // FB.getLoginStatus().  This function gets the state of the
-    // person visiting this page and can return one of three states to
-    // the callback you provide.  They can be:
-    //
-    // 1. Logged into your app ('connected')
-    // 2. Logged into Facebook, but not your app ('not_authorized')
-    // 3. Not logged into Facebook and can't tell if they are logged into
-    //    your app or not.
-    //
-    // These three cases are handled in the callback function.
-
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
-
-};
-
-// Load the SDK asynchronously
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-// Here we run a very simple test of the Graph API after login is
-// successful.  See statusChangeCallback() for when this call is made.
-function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-        console.log('Successful login for: ' + response.name);
-    });
-}
 </script>
 
 <header>
@@ -104,8 +106,7 @@ function testAPI() {
                 aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
-        </fb:login-button>
+
         <div id='navbarText' class='collapse navbar-collapse '>
             <ul class="navbar-nav mr-auto">
                 <!-- Barra de pesquisa que aparece para celulares -->
@@ -126,30 +127,15 @@ function testAPI() {
                 </li>
             </ul>
             <ul class='navbar-nav'>
-                <?php
-                if(empty($_SESSION['logado'])) {
-                    echo "
-                    <li class='nav-item'>
-                        <button class='btn btn-primary' style='margin-left: 10px; margin-right: 10px;' class='nav-link' onclick='openModal()' href='http://localhost/INF221/pages/user.php'>
-                            Login
-                        </button>
-                    </li>";
-                }
-                else {
-                    echo "
-                    <li class='nav-item'>
-                        <a class='nav-link' style='margin-left: 5px; margin-right: 5px;' href='http://localhost/INF221/pages/user.php'>
-                            Minha Conta
-                        </a>
-                    </li>
-                    <li class='nav-item'>
-                        <a class='nav-link' style='margin-left: 5px; margin-right: 10px;' href='http://localhost/INF221/src/modules/logoff.php'>
-                            Sair
-                        </a>
-                    </li>
-                    ";
-                }
-                ?>
+                <li class='nav-item'>
+                    <a id='account' class='nav-link' style='margin-left: 5px; margin-right: 5px; visibility: hidden' href='http://localhost/INF221/pages/user.php'>
+                        Minha Conta
+                    </a>
+                </li>
+                <li class='nav-item' align="center">
+                    <fb:login-button class='btn' style='margin: auto;' autologoutlink="true" scope="public_profile,email" onlogin="checkLoginState();">
+                    </fb:login-button>
+                </li>
                 <!-- Barra de pesquisa para computadores -->
                 <li class='nav-item d-none d-lg-block'>
                     <form class="form-inline my-2 my-lg-0" action='../pages/disciplinas.php' method='POST'>
